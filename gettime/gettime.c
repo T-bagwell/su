c976d1522f5377bb2de2906eb1916b059af7fa0d
Steven Liu <lingjiujianke@gmail.com>
2013-11-06 14:30:37 +0800

    add gettime test case

---
+++ gettime/gettime.c
@@ -0,0 +1,28 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+uint64_t get_local_time()
+{
+    char buffer[32];
+    time_t timep;
+    struct tm *p;
+    time(&timep);
+    p = localtime(&timep);
+    memset(buffer, 0, sizeof(buffer));
+    snprintf(buffer, sizeof(buffer), "%0.4d%0.2d%0.2d%0.2d%0.2d%0.2d",
+             1900 + p->tm_year, 1 + p->tm_mon, p->tm_mday,
+             p->tm_hour, p->tm_min, p->tm_sec);
+    return strtoul(buffer, NULL, 10);
+}
+
+int main()
+{
+    printf("time = [%llu]\n", get_local_time());
+    return 0;
+}
