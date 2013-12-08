038922af1189d61b3a07a52c9c4030bb6ae1a798
Steven Liu <lingjiujianke@gmail.com>
2013-12-08 17:17:41 +0800

    add ctrim trim left spaces test case

---
+++ ctrim/ctrim.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char *ctrim(char *str)
+{
+    char *p = str;
+
+    while (p) {
+        if (*p != ' ' && *p != '\t') {
+            break;
+        }
+        p++;
+    }
+
+    return p;
+}
+
+int main(int argc, char *argv[])
+{
+    char *test_string1 = "   abcde";
+    char *test_string2 = " fuck";
+
+    fprintf(stdout, "test_string1 = [%s], test_string2 = [%s]\n", ctrim(test_string1), ctrim(test_string2));
+    return 0;
+}
